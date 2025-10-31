0470f9f9cf80f0685b2e3102ec6fc8b7c5923ce2
sjdhome <sjdhoome@gmail.com>
2025-10-31 09:58:56 +0800

    Disable auto brackets in blink.cmp

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,6 +1,13 @@
 return {
   "saghen/blink.cmp",
   opts = {
+    completion = {
+      accept = {
+        auto_brackets = {
+          enabled = false,
+        },
+      },
+    },
     sources = {
       per_filetype = {
         markdown = {},
