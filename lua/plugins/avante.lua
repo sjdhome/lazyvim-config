ebe0944a7817d0d7b47464d86a1904844de80bf9
sjdhome <sjdhoome@gmail.com>
2025-11-03 11:30:14 +0800

    Add avante.nvim

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,15 @@
+return {
+  "yetone/avante.nvim",
+  opts = {
+    instructions_file = "AGENTS.md",
+    provider = "openrouter",
+    providers = {
+      openrouter = {
+        __inherited_from = "openai",
+        endpoint = "https://openrouter.ai/api/v1",
+        api_key_name = "OPENROUTER_API_KEY",
+        model = "x-ai/grok-code-fast-1",
+      },
+    }
+  },
+}
