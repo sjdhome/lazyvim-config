0099bbd12e4615783e7cc35d2abc0aae0ac04e6e
sjdhome <sjdhoome@gmail.com>
2025-11-26 10:55:23 +0800

    Fix wrong background color and re-enable avante.nvim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,6 +3,19 @@ return {
     "ellisonleao/gruvbox.nvim",
     opts = {
       contrast = "hard",
+      overrides = {
+        SignColumn = { bg = "#f9f5d7" }, -- light0_hard
+        GruvboxRedSign = { fg = "#9d0006", bg = "#f9f5d7" },
+        GruvboxGreenSign = { fg = "#79740e", bg = "#f9f5d7" },
+        GruvboxYellowSign = { fg = "#b57614", bg = "#f9f5d7" },
+        GruvboxBlueSign = { fg = "#076678", bg = "#f9f5d7" },
+        GruvboxPurpleSign = { fg = "#8f3f71", bg = "#f9f5d7" },
+        GruvboxAquaSign = { fg = "#427b58", bg = "#f9f5d7" },
+        GruvboxOrangeSign = { fg = "#af3a03", bg = "#f9f5d7" },
+        NormalFloat = { fg = "#3c3836", bg = "#f9f5d7" },
+        AvanteSidebarWinSeparator = { fg = "#bdae93", bg = "#f9f5d7" },  -- light3
+        AvanteSidebarWinHorizontalSeparator = { fg = "#bdae93", bg = "#f9f5d7" },
+      },
     },
   },
   {
