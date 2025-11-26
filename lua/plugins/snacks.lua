422563a224ee55acfd1f72fe456be8689ed8b51c
sjdhome <sjdhoome@gmail.com>
2025-11-26 10:27:23 +0800

    Hide search bar in the explorer of snacks.nvim

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        layouts = {
+          explorer_no_input = {
+            layout = {
+              backdrop = false,
+              width = 40,
+              min_width = 40,
+              height = 0,
+              position = "left",
+              border = "none",
+              box = "vertical",
+              { win = "list", border = "none" },
+            },
+          },
+        },
+        sources = {
+          explorer = {
+            layout = "explorer_no_input",
+          },
+        },
+      },
+    },
+  },
+}
