f84d6fddc749c6d1c3a2c7ebf1ac027ccbc7ef6d
sjdhome <sjdhoome@gmail.com>
2025-11-28 09:57:09 +0800

    Enable OSC52

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,16 @@
 -- Add any additional options here
 
 vim.g.autoformat = false
+
+local osc52 = require("vim.ui.clipboard.osc52")
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = osc52.copy("+"),
+    ["*"] = osc52.copy("*"),
+  },
+  paste = {
+    ["+"] = osc52.paste("+"),
+    ["*"] = osc52.paste("*"),
+  },
+}
