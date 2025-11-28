ae68502c05baf9ddb7cda776da5859fba9ccab03
sjdhome <sjdhoome@gmail.com>
2025-11-28 12:47:29 +0800

    Add CSS support

---
+++ lua/plugins/css-lsp.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "css-lsp",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        cssls = {},
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "css",
+      })
+    end,
+  },
+}
